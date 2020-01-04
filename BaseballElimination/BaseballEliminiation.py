a06737d22be103139739efd8d4d8abd259e6cc5f
Vaastav Arora <arora74@purdue.edu>
2020-01-04 17:21:56 -0500

    minor fixes

--- BaseballElimination/BaseballEliminiation.py
+++ BaseballElimination/BaseballEliminiation.py
@@ -1,22 +1,23 @@
 class BaseballElimination:
-    def __init__(self,filename):
+    def __init__(self, filename):
         self.file = open(filename)
         self.teams = int(self.file.readline())
-        self.processFile()
-
-    def processFile(self):
-
         c = self.teams
         self.winCount = [0 for x in range(c)]
         self.loseCount = [0 for x in range(c)]
         self.remainingCount = [0 for x in range(c)]
         self.gameGrid = [[0 for x in range(c)] for y in range(c)]
+        self.teamToIndex = dict()
+        self.indexToTeam = [0 for x in range(c)]
+        self.process_file()
+
+    def process_file(self):
+
+        c = self.teams
 
         count = 0
-        self.teamToIndex = dict()
-        self.indextoTeam = [0 for x in range(c)]
         ti = self.teamToIndex
-        it = self.indextoTeam
+        it = self.indexToTeam
 
         for x in range(c):
             line = self.file.readline().split()
@@ -31,28 +32,28 @@ class BaseballElimination:
                 self.gameGrid[count][y] = int(line[4+y])
             count += 1
 
-    def numberOfTeams(self):
+    def number_of_teams(self):
         return self.indextoTeam
 
-    def wins(self,team: str):
+    def wins(self, team: str):
         if team in self.teamToIndex:
             return self.winCount[self.teamToIndex[team]]
         else:
             return "Team does not exist"
 
-    def losses(self,team: str):
+    def losses(self, team: str):
         if team in self.teamToIndex:
             return self.loseCount[self.teamToIndex[team]]
         else:
             return "Team does not exist"
 
-    def remaining(self,team: str):
+    def remaining(self, team: str):
         if team in self.teamToIndex:
             return self.remainingCount[self.teamToIndex[team]]
         else:
             return "Team does not exist"
 
-    def against(self,team1:str, team2:str):
+    def against(self, team1: str, team2: str):
 
         if team1 not in self.teamToIndex:
             return "First team does not exist"
@@ -62,22 +63,28 @@ class BaseballElimination:
 
         return self.gameGrid[self.teamToIndex[team1]][self.teamToIndex[team2]]
 
-    def printInfo(self):
+    def print_info(self):
 
-        maxlen = 0
-        teams = self.indextoTeam
+        max_len = 0
+        teams = self.indexToTeam
         for x in teams:
-            maxlen = max(maxlen,len(x))
+            max_len = max(max_len,len(x))
 
-        maxlen+= 2
+        max_len += 2
         for x in teams:
             temp = [p1.against(x, w) for w in teams]
             ss = ""
             for w in temp:
                 ss += str(w) + " "
-            sp = " " *(maxlen-len(x))
-            print(x + sp + str(p1.wins(x)) + " " + str(p1.losses(x)) + " " + str(p1.remaining(x)) + "  " + ss)
+            sp = " " *(max_len-len(x))
+            print(x + sp + str(self.wins(x)) + " " + str(self.losses(x)) + " " + str(self.remaining(x)) + "  " + ss)
+
+    def is_eliminated(self, team: str):
+        pass
+
+    def certificate_of_elimination(self, team: str):
+        pass
 
 
 p1 = BaseballElimination("team4")
-p1.printInfo()
+p1.print_info()
